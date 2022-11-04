046a11310b6305a48f7e8e1c8528b338627dbced
Ifeanyi Osi-Okeke <75077705+ifeanyiosi@users.noreply.github.com>
2022-11-04 02:55:44 +0100

    Update index.jsx

--- src/components/utils/Item/index.jsx
+++ src/components/utils/Item/index.jsx
@@ -20,8 +20,11 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
               <h1 className="text-black text-sm font-medium">${price}</h1>
             </div>
             <div className="flex  items-center gap-1 ">
-              <AiFillStar style={{ Color: 'white' }} className="h-6 w-6 sm:h-5 sm:w-5 cursor-pointer
-              bg-white transition-all duration-100 active:scale-110 leading-tight text-slate-900 md:w-4 md:h-4" />
+              <AiFillStar
+                style={{ Color: "white" }}
+                className="h-6 w-6 sm:h-5 sm:w-5 cursor-pointer
+              bg-white transition-all duration-100 active:scale-110 leading-tight text-slate-900 md:w-4 md:h-4"
+              />
               <h1 className="md:text-sm font-normal text-slate-100">
                 {rating}
               </h1>
@@ -29,7 +32,7 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
           </div>
           <div className="flex items-center gap-3">
             <Button
-              style="bg-white px-7 py-1 rounded active: scale-90 transition-all duration-100 ease-in-out shadow-md sm:text-sm bg-white/50 bg-clip-padding backdrop-filter backdrop-blur bg-opacity-50 opacity-100 z-[100] shadow-sm shadow-slate-100 transition-all duration-300 p-[0.5] shadow shadow-sky-200 flex items-center justify-center"
+              style={`bg-white px-7 py-1 rounded active: scale-90 transition-all duration-100 ease-in-out shadow-md sm:text-sm bg-white/50 bg-clip-padding backdrop-filter backdrop-blur bg-opacity-50 opacity-100 z-[100] shadow-sm shadow-slate-100 transition-all duration-300 p-[0.5] shadow shadow-sky-200 flex items-center justify-center`}
               prefix={
                 <AiFillShopping className="h-6 w-6 sm:h-5 sm:w-5 cursor-pointer transition-all duration-100 active:scale-110 leading-tight text-slate-900" />
               }
@@ -39,7 +42,11 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
           </div>
         </div>
         <div>
-          <img className="h-36 w-64 trasitions-theme hover:-rotate-12" src={img} alt="img/item-img" />
+          <img
+            className="h-36 w-64 trasitions-theme hover:-rotate-12"
+            src={img}
+            alt="img/item-img"
+          />
         </div>
       </div>
     </>
