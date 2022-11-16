e2ba90d60a7ed2abde95f5bd31584d60d606a31c
Ifeanyi Osi-Okeke <75077705+ifeanyiosi@users.noreply.github.com>
2022-11-16 23:26:22 +0100

    update

--- src/components/FlexContent/index.jsx
+++ src/components/FlexContent/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Button from "../Button";
 
-const FlexContent = ({ endpoint: { heading, title, text, btn, url, img } }) => {
+const FlexContent = ({ ifExists, endpoint: { heading, title, text, btn, url, img } }) => {
   return (
     <>
       <div
-        className={`flex items-center justify-between lg:flex-col lg:justify-center nike-container`}
+        className={`flex items-center justify-between lg:flex-col lg:justify-center nike-container ${ifExists ? 'flex-row-reverse' : 'flex-row'}`}
       >
-        <div className="max-w-lg lg:max-w-none w-full md:text-center">
+        <div className="max-w-lg lg:max-w-none w-full md:text-center grid items-center lg:justify-items-center">
           <h1 className="text-4xl sm:text-3xl font-bold text-gradient ">
             {heading}
           </h1>
@@ -16,11 +16,11 @@ const FlexContent = ({ endpoint: { heading, title, text, btn, url, img } }) => {
           </h1>
           <p className="xl:text-sm my-4 text-slate-900"> {text} </p>
           <a href={url} className="flex items-center" target={"_blank"} role="button">
-            <Button label={btn} />
+            <Button style="bg-slate-900 shadow-slate-9 text-slate-100 py-1.5" label={btn} />
           </a>
         </div>
-        <div>
-          <img src={img} alt={`img/${heading}`} />
+        <div className="flex items-center justify-center max-w-xl relative lg:max-w-none w-full">
+          <img className={`w-auto object-fill transition-theme `} src={img} alt={`img/${heading}`} />
         </div>
       </div>
     </>
